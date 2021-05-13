4d41b1fc3e5341a567c12ed837ea44e4b5c1480f
Netanel Mizrahi <nmiz1987@gmail.com>
2021-05-13 23:43:29 +0300

    add telegram bot

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import urllib.request
 import json
+import requests
 import time
 import os
-
+from os import environ
 
 # The screen clear function
 def screen_clear():
@@ -13,25 +14,39 @@ def screen_clear():
         # for windows platfrom
         _ = os.system('cls')
 
-
-last_alert = []
+last_alert = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] # 20 slots
+first_run = True
 
 loc = ['גיאה', 'בית שקמה', 'תלמי יפה', 'בת הדר']
 url = 'https://www.oref.org.il/WarningMessages/History/AlertsHistory.json'
+base = environ['key']
 
-while True:
-    response = urllib.request.urlopen(url);
-    data = json.loads(response.read().decode("utf-8"))
-    for i in data[:20]:
-        if i.get('data') in loc:
-            print(i.get('alertDate') + '\t' + i.get('data'))
-            if i.get('alertDate') not in last_alert:
-                last_alert.append(i.get('alertDate'))
-                print('@' * 40)
+try:
+    while True:
+        response = urllib.request.urlopen(url);
+        data = json.loads(response.read().decode("utf-8"))
+        # my zone
+        for i in data[:40]:
+            if i.get('data') in loc: # if in my zone
+                if i not in last_alert: # if new alert
+                    last_alert.pop(0) # pop from the list
+                    last_alert.append(i) # add to the list
+                    if first_run == False:
+                        msg_time = i.get('alertDate')[11:]
+                        info = "צבע אדום!\nשעה " + msg_time + ':\t מיקום:' + str(i.get('data')).lstrip()
+                        requests.get(base + info)
+                        print('@' * 40)
 
-    print('-' * 40)
-    print("All alerts:")
-    for i in data[:11]:
-        print(i.get('alertDate') + '\t' + i.get('data'))
-    time.sleep(60)
-    screen_clear()
+        print('-' * 40)
+        # all zones
+        for i in data[:21]:
+            if first_run == False:
+                # msg_time = i.get('alertDate')[11:]
+                info = i.get('alertDate') +'\t'+ i.get('data')
+                # requests.get(base + info)
+                print(info)
+        first_run = False
+        time.sleep(5)
+        screen_clear()
+finally:
+    pass
