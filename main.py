29e3af730ee2eafa52e1f5c30ab8ae33d3d6726a
Netanel Mizrahi <nmiz1987@gmail.com>
2021-05-13 23:51:50 +0300

    add telegram bot

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ loc = ['גיאה', 'בית שקמה', 'תלמי יפה', 'בת הדר']
 url = 'https://www.oref.org.il/WarningMessages/History/AlertsHistory.json'
 base = environ['key']
 
-requests.get("server start running...")
+requests.get(base + "server start running...")
 
 try:
     while True:
