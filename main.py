010b5cbd99665e78e8f8c1bb6712708c847e864c
Netanel Mizrahi <nmiz1987@gmail.com>
2021-05-14 14:55:12 +0300

    how about now?

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ url = 'https://www.oref.org.il/WarningMessages/History/AlertsHistory.json'
 base = environ['key']
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-
 refresh = 3
 
 requests.get(base + "server start running...")
